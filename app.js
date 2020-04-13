2ba17c56969fb2ab00ee919d93ae392204624a6c
Shawn <pan.conan@gmail.com>
2020-04-13 11:50:15 +0800

    connect with DB

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const db = require('./models')
 
 app.listen(3000, ()=>{
+    db.sequelize.sync() //確認DB連線正常
     console.log('localhost:3000 is listenting')
 })
 
